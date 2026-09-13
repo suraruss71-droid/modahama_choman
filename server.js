3eb981db0d53170eef7bafdec7029308df4629d2
suraruss71-droid <suraruss71@gmail.com>
2026-09-13 13:06:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
-const express = require('http');
+const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+// بڵاوکردنەوەی فایلە HTML و فایلە ستاتیکەکان 📂
+app.use(express.static(path.join(__dirname)));
 
 let units = {};
 
 io.on('connection', (socket) => {
     console.log('ئەلفا پەیوەندی بە سێرڤەرەوە کرد:', socket.id);
+
+    // ناردنەوەی دوایین لیستی یەکەکان بۆ ئەوەی هەر کەسێک نوێبوەوە زانیارییەکە ببینێت
+    socket.emit('updateUnitsList', units);
 
     socket.on('updateLocation', (data) => {
         units[data.id] = { ...data, lastSeen: Date.now() };
